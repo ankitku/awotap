a052a5ac26ca852f5163a88dc2cb7a69d25f8814
Ankit Kumar <ankitkumar.itbhu@gmail.com>
2016-10-24 06:30:08 +0530

    simplifying code, adding notations

--- TAL.v
+++ TAL.v
@@ -2,56 +2,59 @@
 
 Require Import Bool Arith Vector Maps.
 
-(* 10 registers *)
-Definition K := 10.
 Definition registers := total_map nat.
 Definition empty_regs : registers := t_empty 0.
       
 Inductive aexp : Type :=
-| ANum : nat -> aexp
-| AReg : nat -> aexp
-| APlus : aexp -> aexp -> aexp
-| ASub : aexp -> aexp -> aexp.
+ | ANum : nat -> aexp
+ | AReg : nat -> aexp
+ | ALab : nat -> aexp.
+
 
 (*define relations for aeval , ieval*)
 Fixpoint aeval (a : aexp) (R : registers) : nat :=
   match a with
-  | ANum n => n
-  | AReg d => R (Id d)
-  | APlus a1 a2 => aeval a1 R + aeval a2 R
-  | ASub a1 a2 => aeval a1 R - aeval a2 R                
+ | ANum n => n
+ | AReg d => R (Id d)
+ | ALab l => l
   end.
 
+
 Inductive instr : Type :=
-| IAss : forall d : nat, (d < K) -> aexp -> instr
-| IAdd : forall d s : nat, (d < K) -> (s < K) -> instr
-| ISub : forall d v : nat, (d < K) -> instr
-| IIf : forall d : nat, (d < K) -> aexp -> instr
-| ISeq : instr -> instr -> instr
-| IJmp : aexp -> instr.
+ | IMov : forall d : nat,
+    aexp -> instr
+ | IAdd : forall d s : nat,
+    instr
+ | ISub : forall d v : nat,
+    instr
+ | IIf : forall d : nat,
+    aexp -> instr
+ | ISeq : instr -> instr -> instr
+ | IJmp : aexp -> instr.
 
-(* Unable to use notations as can't construct proof of d < K on the fly :(
 
-Notation "'R(' d ')' '::=' a" :=
-  (IAss d _ (ANum a)) (at level 60).
+Notation "'R(' d ')' ':=' a" :=
+  (IMov d (ANum a)) (at level 60).
 Notation "'R(' s ')' '+:=' 'R(' d ')'" :=
-  (IAdd d s _ _) (at level 60).
+  (IAdd d s) (at level 60).
 Notation "'R(' s ')' '-:=' v" :=
-  (ISub s v _ ) (at level 60).
+  (ISub s v) (at level 60).
 Notation "i1 ;; i2" :=
   (ISeq i1 i2) (at level 80, right associativity).
 Notation "'JIF' 'R(' d ')' v" :=
-  (IIf d _ (ANum v)) (at level 70).
-Notation "'JUMP' v" :=
-  (IJmp (ANum v)) (at level 80).
+  (IIf d (ANum v)) (at level 70).
+Notation "'JMP' v" :=
+  (IJmp (ALab v)) (at level 80).
+Notation "'JMP' 'R(' r ')'" :=
+  (IJmp (AReg r)) (at level 80).
 
 Check JIF R(1) 2.
-Check R(1) ::= 10.
+Check R(1) := 10.
 Check R(2) +:= R(1).
 Check R(2) -:= 1.
 Check R(2) +:= R(1) ;; R(2) -:= 1.
-Check JUMP 2.
-*)
+Check JMP 2.
+Check JMP R(2).
 
       
 Definition heaps := partial_map instr.
@@ -59,28 +62,29 @@ Definition empty_heap : heaps := empty.
 
 (* Machine State *)
 Inductive st : Type :=
-| St : heaps -> registers -> instr -> st.
+ | St : heaps -> registers -> instr -> st.
 
 
 Inductive ieval : st -> st -> Prop :=
-| R_IAss : forall H R I d a1 pf, ieval (St H R (ISeq (IAss d pf a1) I)) (St H (t_update R (Id d) (aeval a1 R)) I)
-| R_IAdd : forall H R I d s pf1 pf2, ieval (St H R (ISeq (IAdd d s pf1 pf2) I)) (St H (t_update R (Id s) (aeval (AReg d) R + aeval (AReg s) R)) I)
-| R_ISub : forall H R I d v pf1, ieval (St H R (ISeq (ISub d v pf1) I)) (St H (t_update R (Id d) (aeval (AReg d) R - aeval (ANum v) R)) I)
-| R_IJmp_Succ : forall H R I' v, H (Id v) = Some I' -> ieval (St H R (IJmp (ANum v))) (St H R I')
-| R_IJmpR_Succ : forall H R I' v, H (Id (R (Id v))) = Some I' -> ieval (St H R (IJmp (ANum v))) (St H R I')
-| R_IJmp_Fail : forall H R I v, H (Id v) = None -> ieval (St H R I) (St H R I)
-| R_IIf_EQ : forall H R I I' v r, aeval (AReg r) R = 0 -> (H (Id v)) = Some I' -> ieval (St H R I) (St H R I')
-| R_IIf_NEQ : forall H R I v r pf, aeval (AReg r) R <> 0 -> ieval (St H R (ISeq (IIf r pf (ANum v)) I)) (St H R I)   
-| R_ISeq : forall st st' st'', ieval st st' -> ieval st' st'' -> ieval st st''
-| R_IStuck : forall st, ieval st st
-.
-(* Proofs (n < K) for registers which are less than K = 10 in number *)
-Definition oneLEten :=  (lt_n_S 0 9 (lt_0_Sn 8)).
-Definition twoLEten :=  (lt_n_S 1 9 (lt_n_S 0 8 (lt_0_Sn 7))).
-Definition threeLEten := (lt_n_S 2 9 (lt_n_S 1 8 (lt_n_S 0 7 (lt_0_Sn 6)))).
-Definition fourLEten := (lt_n_S 3 9 (lt_n_S 2 8 (lt_n_S 1 7 (lt_n_S 0 6 (lt_0_Sn 5))))).
+ | R_IMov : forall H R I d a,
+    ieval (St H R (R(d) := a ;; I)) (St H (t_update R (Id d) a) I)
+ | R_IAdd : forall H R I d s,
+     ieval (St H R (R(d) +:= R(s) ;; I)) (St H (t_update R (Id s) (aeval (AReg d) R + aeval (AReg s) R)) I)
+ | R_ISub : forall H R I d v,
+     ieval (St H R (R(d) -:= v ;; I)) (St H (t_update R (Id d) (aeval (AReg d) R - aeval (ANum v) R)) I)
+ | R_IJmp_Succ : forall H R I' a,
+     H (Id (aeval a R)) = Some I' -> ieval (St H R (JMP (aeval a R))) (St H R I')
+ | R_IJmp_Fail : forall H R I a,
+     H (Id (aeval a R)) = None -> ieval (St H R I) (St H R I)
+ | R_IIf_EQ : forall H R I r v,
+     aeval (AReg r) R = 0 -> (H (Id v)) = Some I -> ieval (St H R (JIF R(r) v)) (St H R I)
+ | R_IIf_NEQ : forall H R I r v,
+     aeval (AReg r) R <> 0 -> ieval (St H R (JIF R(r) v ;; I)) (St H R I)   
+ | R_ISeq : forall st st' st'',
+     ieval st st' -> ieval st' st'' -> ieval st st''
+ | R_IStuck : forall st, ieval st st.
 
-Definition init_heap := (update (update (update empty_heap (Id 1) (ISeq (IAss 3 threeLEten (ANum 0)) (IJmp (ANum 2)))) (Id 2) (ISeq (IIf 1 oneLEten (ANum 3)) (ISeq (IAdd 2 3 twoLEten threeLEten) (ISeq (ISub 1 1 oneLEten) (IJmp (ANum 2))) ))  )  (Id 3) (IJmp (AReg 4)) ).
+Definition init_heap := update (update (update empty_heap (Id 1) (R(3) := 0 ;; JMP 2)) (Id 2) (JIF R(1) 3 ;; R(2) +:= R(3) ;; R(1) -:= 1 ;; JMP 2) ) (Id 3) (JMP R(4)).
 
 Definition init_regs : registers :=  (t_update (t_update  (t_update (t_update (t_update empty_regs (Id 5) 1) (Id 6) 2) (Id 7) 3) (Id 1) 1) (Id 2) 2).
 Definition final_regs : registers := (t_update (t_update (t_update  (t_update (t_update (t_update empty_regs (Id 5) 1) (Id 6) 2) (Id 4) 1) (Id 1) 0) (Id 2) 2) (Id 3) 2).
@@ -89,28 +93,22 @@ Eval compute in init_heap (Id (init_regs (Id 6))).
 
 (* jump to a label proof *)
 
-Example ieval_example1 : ieval (St init_heap init_regs (ISeq (IAss 3 threeLEten (ANum 0)) (IJmp (ANum 2)))) (St init_heap (t_update init_regs (Id 3) 0)
-                          (ISeq (IIf 1 oneLEten (ANum 3))
-                          (ISeq (IAdd 2 3 twoLEten threeLEten)
-                          (ISeq (ISub 1 1 oneLEten) (IJmp (ANum 2)))))).
+Example ieval_example1 : ieval (St init_heap init_regs
+                          (R(3) := 0 ;; JMP 2))
+                               (St init_heap (t_update init_regs (Id 3) 0)
+                          (JIF R(1) 3 ;; R(2) +:= R(3) ;; R(1) -:= 1 ;; JMP 2)).
 Proof.
-  apply R_ISeq with (St init_heap (t_update init_regs (Id 3) 0) (IJmp (ANum 2))).
-  apply R_IAss.
-  apply R_IJmp_Succ with (v := 2).
+  apply R_ISeq with (St init_heap (t_update init_regs (Id 3) 0) (IJmp (ALab 2))).
+  apply R_IMov.
+  apply R_IJmp_Succ with (a := ALab 2).
   reflexivity.
 Qed.
-
-Inductive var : Type :=
-| alpha : var
-| beta : var
-| gam : var.
-          
+     
 Inductive ty : Type :=
-| int : ty
-| reg : ty -> ty
-| code : partial_map ty -> ty
-| arrow : partial_map ty -> partial_map ty -> ty
-| True : ty.
+ | int : ty
+ | code : partial_map ty -> ty
+ | arrow : partial_map ty -> partial_map ty -> ty
+ | True : ty.
 
 
 Definition context := partial_map ty.
@@ -123,41 +121,51 @@ Definition empty_Psi : context := empty.
 
 (* typing rules for arithmetic expressions *)
 Inductive ahas_type : context -> context -> aexp -> ty -> Prop :=
-| S_Reg : forall Psi Gamma (r : nat) tau, r < K -> Gamma (Id r) = Some tau -> ahas_type Psi Gamma (AReg r) (reg tau) 
-| S_Int : forall Psi Gamma (n : nat), ahas_type Psi Gamma (ANum n) int
-| S_Lab : forall Psi Gamma (s : nat) tau, Psi (Id s) = Some tau -> ahas_type Psi Gamma (ANum s) tau.
-    
-  
+ | S_Reg : forall Psi Gamma r tau,
+    Gamma (Id r) = Some tau -> ahas_type Psi Gamma (AReg r) tau 
+ | S_Int : forall Psi Gamma n,
+    ahas_type Psi Gamma (ANum n) int
+ | S_Lab : forall Psi Gamma l tau,
+    Psi (Id l) = Some tau -> ahas_type Psi Gamma (ALab l) tau.
+
 Hint Constructors ahas_type.
 
 (* typing rules for instructions *)
-(* Jmp to Num v is for labels in heap.*)
 Inductive ihas_type : context -> context -> instr -> ty -> Prop :=
-| S_Mov : forall Psi Gamma v tau d pf, ahas_type Psi Gamma v tau -> ahas_type Psi Gamma (AReg d) (reg tau) -> (update Gamma (Id d) tau) = Gamma -> ihas_type Psi Gamma (IAss d pf v) (arrow Gamma Gamma)
-| S_Add : forall Psi Gamma s d pf1 pf2, ahas_type Psi Gamma (AReg s) (reg int) -> ahas_type Psi Gamma (AReg d) (reg int) -> update Gamma (Id d) int = Gamma -> ihas_type Psi Gamma (IAdd d s pf1 pf2) (arrow Gamma Gamma)
-| S_Sub : forall Psi Gamma s n pf, ahas_type Psi Gamma (AReg s) (reg int) -> ahas_type Psi Gamma (ANum n) int -> ihas_type Psi Gamma (ISub s n pf) (arrow Gamma Gamma)
-| S_If :  forall Psi Gamma s v pf1, ahas_type Psi Gamma (AReg s) (reg int) -> ahas_type Psi Gamma (ANum v) (code Gamma) -> ihas_type Psi Gamma (IIf s pf1 (ANum v)) (arrow Gamma Gamma)
-| S_Jmp :  forall Psi Gamma v,  ahas_type Psi Gamma (ANum v) (code Gamma) -> ihas_type Psi Gamma (IJmp (ANum v)) (code Gamma)
-| S_JmpR :  forall Psi Gamma v i,  Gamma (Id i) = Some True -> update Gamma (Id i) True = Gamma -> ahas_type Psi Gamma (AReg v) (reg True) -> ihas_type Psi Gamma (IJmp (AReg v)) (code Gamma)
-| S_Seq :  forall Psi i1 i2 Gamma Gamma2,  ihas_type Psi Gamma i1 (arrow Gamma Gamma2) -> ihas_type Psi Gamma i2 (code Gamma2) -> ihas_type Psi Gamma (ISeq i1 i2) (code Gamma).
+ | S_Mov : forall Psi Gamma R a tau d,
+    ahas_type Psi Gamma a tau -> ahas_type Psi Gamma (AReg d) tau -> (update Gamma (Id d) tau) = Gamma -> ihas_type Psi Gamma (R(d) := aeval a R) (arrow Gamma Gamma)
+ | S_Add : forall Psi Gamma s d,
+    ahas_type Psi Gamma (AReg s) int -> ahas_type Psi Gamma (AReg d) int -> update Gamma (Id d) int = Gamma -> ihas_type Psi Gamma (R(d) +:= R(s)) (arrow Gamma Gamma)
+ | S_Sub : forall Psi Gamma s n,
+     ahas_type Psi Gamma (AReg s) int -> ihas_type Psi Gamma (R(s) -:= n) (arrow Gamma Gamma)
+ | S_If :  forall Psi Gamma r v,
+     ahas_type Psi Gamma (AReg r) int -> ahas_type Psi Gamma (ALab v) (code Gamma) -> ihas_type Psi Gamma (JIF R(r) v) (arrow Gamma Gamma)
+ | S_Jmp :  forall Psi Gamma a R v,
+     ahas_type Psi Gamma a (code Gamma) -> aeval a R = v -> ihas_type Psi Gamma (JMP v) (code Gamma)
+ | S_JmpT :  forall Psi Gamma v r,
+     Gamma (Id r) = Some True -> update Gamma (Id r) True = Gamma -> ahas_type Psi Gamma (AReg v) True -> ihas_type Psi Gamma (IJmp (AReg v)) (code Gamma)
+ | S_Seq :  forall Psi i1 i2 Gamma Gamma2,
+     ihas_type Psi Gamma i1 (arrow Gamma Gamma2) -> ihas_type Psi Gamma i2 (code Gamma2) -> ihas_type Psi Gamma (ISeq i1 i2) (code Gamma).
 
 Hint Constructors ihas_type.
 
+
 (* typing rules for register file *)
 Inductive Rhas_type : context -> registers -> context -> Prop :=
-| S_Regfile : forall r Psi Gamma (R : registers) tau, r < K ->  (Gamma (Id r)) = Some tau -> ahas_type Psi Gamma (AReg r) (reg tau) -> Rhas_type Psi R Gamma.
+ | S_Regfile : forall Psi Gamma R r tau,
+    (Gamma (Id r)) = Some tau -> ahas_type Psi Gamma (AReg r) tau -> Rhas_type Psi R Gamma.
 
 Hint Constructors Rhas_type.
 
 (* typing rules for heap*)
 Inductive Hhas_type : heaps -> context -> Prop :=
-  | S_Heap : forall Psi Gamma (H : heaps) l tau i, Psi (Id l) = Some tau -> H (Id l) = Some i -> ihas_type Psi Gamma i tau -> Hhas_type H Psi.
+  | S_Heap : forall Psi Gamma H l tau i, Psi (Id l) = Some tau -> H (Id l) = Some i -> ihas_type Psi Gamma i tau -> Hhas_type H Psi.
 
 Hint Constructors Hhas_type.
 
 (* typing rules for Machine State *)
 Inductive M_ok : heaps -> registers -> instr -> Prop :=
-| S_Mach : forall H R I Psi Gamma, Hhas_type H Psi -> Rhas_type Psi R Gamma -> ihas_type Psi Gamma I (code Gamma) -> M_ok H R I.
+ | S_Mach : forall H R I Psi Gamma, Hhas_type H Psi -> Rhas_type Psi R Gamma -> ihas_type Psi Gamma I (code Gamma) -> M_ok H R I.
 
 Hint Constructors M_ok.
 
@@ -170,9 +178,9 @@ Definition init_Psi : context := update (update (update empty_Psi (Id 1) (code i
 
 Ltac match_map := repeat (try rewrite update_neq; try rewrite update_eq; try reflexivity).
 Ltac inequality := (rewrite <- beq_id_false_iff; trivial).
-Ltac crush_map := match_map; inequality.
+Ltac crush_map := match_map ; inequality.
 
-Lemma heap_2_type : forall I, (init_heap (Id 2)) = Some I -> ihas_type init_Psi init_Gamma I (code init_Gamma).
+Lemma heap_2_type : forall I (R : registers), (init_heap (Id 2)) = Some I -> ihas_type init_Psi init_Gamma I (code init_Gamma).
 Proof.
   intros.
   unfold init_heap in H.
@@ -183,20 +191,17 @@ Proof.
   apply S_Seq with (Gamma2 := init_Gamma).
   apply S_If.
   apply S_Reg.
-  apply oneLEten.
   unfold init_Gamma.
-  crush_map.        
+  crush_map.
   apply S_Lab.
   unfold init_Psi.
   crush_map.
   apply S_Seq with (Gamma2 := init_Gamma).
   apply S_Add.
   apply S_Reg.
-  apply threeLEten.
   unfold init_Gamma.
   crush_map.
   apply S_Reg.
-  apply twoLEten.
   unfold init_Gamma.
   crush_map.
   apply update_same.
@@ -205,14 +210,13 @@ Proof.
   apply S_Seq with (Gamma2 := init_Gamma).
   apply S_Sub.
   apply S_Reg.
-  apply oneLEten.
   unfold init_Gamma.
   crush_map.
-  apply S_Int.
-  apply S_Jmp.
+  apply S_Jmp with (a := ALab 2) (R := init_regs).
   apply S_Lab.
   unfold init_Psi.
   apply update_eq.
+  crush_map.
   rewrite <- beq_id_false_iff.
   trivial.  
 Qed.
@@ -224,68 +228,48 @@ Proof.
   rewrite update_eq in H.
   symmetry in H.
   inversion H.
-  apply S_JmpR with (i := 4).
+  apply S_JmpT with (r := 4).
   unfold init_Gamma.
   apply update_eq.
   apply update_same.
   unfold init_Gamma.
   apply update_eq.
   apply S_Reg.
-  apply fourLEten.
   unfold init_Gamma.
   apply update_eq.
 Qed.
 
-Lemma Canonical_Values_int : forall H Psi Gamma v tau, Hhas_type H Psi -> ahas_type Psi Gamma v tau -> tau = int -> exists n, v = ANum n.
+Lemma Canonical_Values_Int : forall H Psi Gamma v tau, Hhas_type H Psi -> ahas_type Psi Gamma v tau -> tau = int -> exists n, v = ANum n.
 Proof.
   intros.
   inversion H1.
   subst.
-  exists 0.
+  exists r.
   inversion H1.
   exists n; reflexivity.
   exists s; reflexivity.
 Qed.
 
-Lemma Canonical_Values_reg :forall H Psi Gamma r R, Hhas_type H Psi -> Rhas_type Psi R Gamma -> ahas_type Psi Gamma (AReg r) (reg int) -> exists n, R (Id r) = n.
+Lemma Canonical_Values_Reg :forall H Psi Gamma r R tau v, Hhas_type H Psi -> Rhas_type Psi R Gamma -> v = AReg r -> ahas_type Psi Gamma v tau -> tau = (reg int) -> exists n, R (Id r) = n.
 Proof.
   intros.
-  inversion H1.
   exists (R (Id r)).
   reflexivity.
 Qed.
 
-
-Lemma Canonical_Values_label1 : forall H Psi Gamma r R, Hhas_type H Psi -> ahas_type Psi Gamma (AReg r) (code Gamma) -> exists i l, ihas_type Psi Gamma i (code Gamma) -> l = R (Id r) -> Some i = H (Id l).
+Lemma Canonical_Values_label1 : forall H Psi Gamma r R, Hhas_type H Psi -> ahas_type Psi Gamma (AReg r) (code Gamma) -> exists i, ihas_type Psi Gamma i (code Gamma) -> exists l, l = R (Id r) -> Some i = H (Id l).
 Proof.
   intros.
   inversion H0.
-  exists i.
-  exists l.
-  intros.
-  inversion H8.
-  rewrite H13.
-  symmetry in H4.
-  assumption.
-
-  rewrite H15.
-  symmetry in H4.
-  assumption.
-
-  rewrite H14.
-  symmetry in H4.
-  assumption.
+  inversion H1.
 Qed.
 
-Lemma Canonical_Values_label2 : forall H Psi Gamma v, Hhas_type H Psi -> ahas_type Psi Gamma (ANum v) (code Gamma) -> exists i l, v = l -> Some i = H (Id v).
+Lemma Canonical_Values_label2 : forall H Psi Gamma v, Hhas_type H Psi -> ahas_type Psi Gamma (ANum v) (code Gamma) -> exists i, ihas_type Psi Gamma i (code Gamma) -> exists l,  Some i = H (Id v) -> v = l.
 Proof.
   intros.
   inversion H0.
-  exists i.
-  exists l.
-  intros.
-  symmetry in H4.
-  rewrite H8.
+  inversion H1.
+
   assumption.
 Qed.
 
