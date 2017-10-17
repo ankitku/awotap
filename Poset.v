1354a1f0e2f77c0157398553e666b6ff0be6d1ee
Ankit Kumar <ankitkumar.itbhu@gmail.com>
2017-10-18 01:11:59 +0530

    reduced clutter, cleaning

--- Poset.v
+++ Poset.v
@@ -31,61 +31,54 @@ Section Poset_Properties.
            | [ H : _ /\ _ |- _ ] => destruct H
            | [ |- nat -> _     ] => intro
            | [ H : Lower_Bounded_Subset _ |- _ ] => unfold Lower_Bounded_Subset in H
-           | [ H : In _ _ |- _ ] => unfold In in H
-           | _ =>tauto
+           | [ H : Upper_Bounded_Subset _ |- _ ] => unfold Upper_Bounded_Subset in H
+           | [ H : In _ _ _ |- _ ] => unfold In in H
+           | [ H : Included _ _ _ |- _ ] => unfold Included in H
+           | _ => tauto
            end.
 
+  Ltac unfold_all := try unfold Included;
+                     try unfold Above;
+                     try unfold Below;
+                     try unfold In;
+                     try unfold Lower_Bounded_Subset;
+                     try unfold Upper_Bounded_Subset.
+  
   Ltac crush :=
-    repeat (crush_generic; match goal with
-                           | [ |- Lower_Bounded_Subset _ ] => unfold Lower_Bounded_Subset
-                           | [ |- Upper_Bounded_Subset _ ] => unfold Upper_Bounded_Subset
-                           | [ |- ?T -> False  ]  => assert T
-                           | _ => try trivial
-                           end).
+    repeat (crush_generic;
+            unfold_all;
+            match goal with
+            | [ |- ?T -> False  ]  => assert T
+            | _ => try trivial
+            end).
 
-  Lemma Below_In_C : forall  (A : Ensemble U), Included _ A C -> Included _ (Below A) C.
+  Lemma Below_In_Carrier : forall  (A : Ensemble U), Included _ A C -> Included _ (Below A) C.
   Proof.
-    unfold Included.
-    unfold In.
-    unfold Below.
-    intros.
-
     crush.
   Qed.
-  
+
+  Lemma Above_In_Carrier : forall  (A : Ensemble U), Included _ A C -> Included _ (Above A) C.
+  Proof.
+    crush.
+  Qed.
+
+  (** Lower Bound (Set) of a nonempty subset is Upper Bounded *)
   Lemma Bound_Invert : forall  (A : Ensemble U), Included _ A C /\ Inhabited _ A /\ Lower_Bounded_Subset A -> Upper_Bounded_Subset (Below A).
   Proof.
-    intros.
     crush.
+    intros.
 
-    inversion H0.
-    inversion H1.
+    crush. inversion H0. inversion H1.
     
     apply Inhabited_intro with (x := x).
-    unfold In.
-
-    unfold Above.
-    unfold Below.
-    unfold In.
-    split.
+    crush. apply H. tauto.
     
-    unfold In in H2.
-
-    unfold Included in H.
-    apply H.
-    tauto.
-    
-    intros.
-    unfold In in H2.
-    unfold In in H3.
-    destruct H4.
-    specialize H5 with x.
-    tauto.
+    intros. crush.
+    specialize H6 with x. tauto.
   Qed.
 
-  (* If every nonempty upper-bounded subset has a least upper bound, then
+  (** If every nonempty upper-bounded subset has a least upper bound, then
 every nonempty lower-bounded subset has a greatest lower bound. *)
-  
   Theorem Th : (forall A : Ensemble U, Included _ A C /\ Inhabited _ A /\ Upper_Bounded_Subset A -> exists lub, Lub _ P A lub) -> (forall B : Ensemble U, Included _ B C /\ Inhabited _ B /\ Lower_Bounded_Subset B -> exists glb, Glb _ P B glb).
   Proof.
     intros.
@@ -93,48 +86,26 @@ every nonempty lower-bounded subset has a greatest lower bound. *)
     pose proof Bound_Invert B H0 as R.
 
     crush.
-    pose proof Below_In_C B H0 as T.
+    pose proof Below_In_Carrier B H0 as T.
     pose proof H (conj T (conj H2 R)) as S.
     destruct S.
     exists x.
 
     apply Glb_definition.
-    inversion H2.
 
-    inversion H3.
-    apply Lower_Bound_definition.
-    inversion H5.
-    assumption.
+    inversion H3. apply Lower_Bound_definition.
+    inversion H4. assumption.
 
-    intros.
-    specialize H6 with y.
-    apply H6.
-
+    intros. specialize H5 with y. apply H5.
     apply Upper_Bound_definition.
-    unfold Included in H0.
-    specialize H0 with y.
-    tauto.
+    specialize H0 with y. tauto.
 
-    intros.
-    unfold Below in H8.
-    unfold In in H8.
-    crush.
-    apply H9.
-    tauto.
+    intros. crush. unfold Below in H7.
+    apply H7. tauto.
 
-    intros.
-    inversion H3.
-    inversion H5.
-    specialize H8 with y.
-    apply H8.
+    intros. inversion H3. inversion H5.
+    specialize H8 with y. apply H8.
 
-    unfold Below.
-    unfold In.
-    inversion H4.
-    split.
-
-    assumption.
-
-    assumption.
+    crush; inversion H4; assumption.
   Qed.
   
